0366d9a2da5395b90e8b9c7bb79bcbf089fb6b7f
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-09-06 02:12:11 +0000

    small fixes

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from rich.console import Console
 from rich.prompt import Confirm
-
+from math import sqrt, pow, log, log10, log2
 global console
 import os
 
-from dotenv import load_dotenv
+# from dotenv import dotenv_values
+# temp = dotenv_values(".env")
 
-load_dotenv()
 console = Console()
 
 
@@ -14,7 +14,9 @@ def login():
     console.print(f"[green bold]Attempt {attempt}[/]")
     un = console.input("Username: ")
     pw = console.input("Password: ", password=True)
-    if un == os.environ["USERNAME"] and pw == os.environ["PASSWORD"]:
+    # if un == temp["USERNAME"] and pw == temp["PASSWORD"]:
+    # if un == os.environ["USERNAME"] and pw == os.environ["PASSWORD"]:
+    if un == "admin" and pw == "admin":
         console.rule()
         console.log("Logged In ...")
         console.print("[green bold]Welcome Back[/]", justify="center")
@@ -25,7 +27,7 @@ def login():
         login()
 
 
-def cont():
+def continue_it():
     if Confirm("Do you want to continue?"):
         global count
         count += 1
@@ -57,7 +59,7 @@ def cont():
                 login()
 
 
-def printname():
+def print_name():
     console.print("[bold]Print Your Name:[/]")
     i = input("Enter Your Name : ")
     console.print("Your Name       :", i)
@@ -65,17 +67,9 @@ def printname():
 
 
 def calcu():
-    console.print("[bold]Calculator:[/]]")
-    calcu1 = int(input("Enter the 1st number : "))
-    calcu2 = int(input("Enter the 2nd number : "))
-    console.rule(characters="-")
-    console.print("Sum        : ", calcu1 + calcu2)
-    console.rule(characters="-")
-    console.print("Difference : ", calcu1 - calcu2)
-    console.rule(characters="-")
-    console.print("Product    : ", calcu1 * calcu2)
-    console.rule(characters="-")
-    console.print("Quotient   : ", calcu1 / calcu2)
+    console.print("[bold]Calculator:[/]")
+    expression = str(console.input("Enter the expression: "))
+    console.print(f"[bold]Answer: {eval(expression)}[/]")
     console.rule()
 
 
@@ -110,35 +104,35 @@ def cap():
         ["111111", "   11 ", "  11  ", " 11   ", "111111"],
         ["      ", "      ", "      ", "      ", "      "],
     ]
-    a2 = [
-        ["a", "A", 0],
-        ["b", "B", 1],
-        ["c", "C", 2],
-        ["d", "D", 3],
-        ["e", "E", 4],
-        ["f", "F", 5],
-        ["g", "G", 6],
-        ["h", "H", 7],
-        ["i", "I", 8],
-        ["j", "J", 9],
-        ["k", "K", 10],
-        ["l", "L", 11],
-        ["m", "M", 12],
-        ["n", "N", 13],
-        ["o", "O", 14],
-        ["p", "P", 15],
-        ["q", "Q", 16],
-        ["r", "R", 17],
-        ["s", "S", 18],
-        ["t", "T", 19],
-        ["u", "U", 20],
-        ["v", "V", 21],
-        ["w", "W", 22],
-        ["x", "X", 23],
-        ["y", "Y", 24],
-        ["z", "Z", 25],
-        [" ", "  ", 26],
-    ]
+    a2 = {
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 3,
+        "e": 4,
+        "f": 5,
+        "g": 6,
+        "h": 7,
+        "i": 8,
+        "j": 9,
+        "k": 10,
+        "l": 11,
+        "m": 12,
+        "n": 13,
+        "o": 14,
+        "p": 15,
+        "q": 16,
+        "r": 17,
+        "s": 18,
+        "t": 19,
+        "u": 20,
+        "v": 21,
+        "w": 22,
+        "x": 23,
+        "y": 24,
+        "z": 25,
+        " ": 26,
+    }
     a3 = []
     a4 = ["", "", "", "", ""]
     alpha = console.input("Enter Your Name : ")
@@ -151,18 +145,16 @@ def cap():
     leng = len(alpha)
     if choice == 1:
         for l in range(0, leng):
-            for k in range(0, 27):
-                if alpha[l] == a2[k][0] or alpha[l] == a2[k][1]:
-                    a3.append(a2[k][2])
+            if alpha[l] in a2:
+                a3.append(a2[k])
         for i in range(0, len(a3)):
             for j in range(0, 5):
                 console.print(a1[a3[i]][j])
             console.print()
     elif choice == 2:
         for l in range(0, leng):
-            for k in range(0, 27):
-                if alpha[l] == a2[k][0] or alpha[l] == a2[k][1]:
-                    a3.append(a2[k][2])
+            if alpha[l] in a2:
+                a3.append(a2[k])
         for j in range(0, 5):
             for i in range(0, len(a3)):
                 a4[j] += a1[a3[i]][j]
@@ -187,20 +179,21 @@ def prh():
 4) Quit
 Enter Your Choice: """
     )
-    console.rule()
-    ch = int(ch)
-    if ch == 1:
-        printname()
-    elif ch == 2:
-        calcu()
-    elif ch == 3:
-        cap()
-    elif ch == 4:
-        quit()
-    else:
-        console.print("Invalid Choice...")
+    while True:
         console.rule()
-    cont()
+        ch = int(ch)
+        if ch == 1:
+            print_name()
+        elif ch == 2:
+            calcu()
+        elif ch == 3:
+            cap()
+        elif ch == 4:
+            quit()
+        else:
+            console.print("Invalid Choice...")
+            console.rule()
+        continue_it()  
 
 
 if __name__ == "__main__":
